6e3b16b28e7d3ecf4a852666ed90bd6966305855
devjun10 <devjun1023@gmail.com>
2024-04-02 03:19:33 +0900

    test: 날씨 예제 단위 테스트 작성 - 내용 보강

--- app/src/test/kotlin/project/gson/app/test/unittest/ExampleUnitTest.kt
+++ app/src/test/kotlin/project/gson/app/test/unittest/ExampleUnitTest.kt
@@ -1,12 +1,15 @@
 package project.gson.app.test.unittest
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.io.File
 
+@DisplayName("[UnitTest] 날씨 예제 단위 테스트")
 class ExampleUnitTest {
 
     @Test
+    @DisplayName("Json 파일에 데이터를 추가할 수 있다.")
     fun trafficAdditionalTest() {
         val filePath = "PATH"
 
